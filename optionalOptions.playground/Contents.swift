5f5a651065a1734bbe1bc1f98de8773523b235af
West Kraemer <kraemerwest@gmail.com>
2018-09-20 20:33:17 -0400

    add comments describing examples

--- optionalOptions.playground/Contents.swift
+++ optionalOptions.playground/Contents.swift
@@ -3,7 +3,14 @@ import UIKit
 var optionalNumber: Int? = 5
 var number: Int = 5
 
+//Force Unwrapping
 if optionalNumber != nil {
     print ("optionalNumber has a value of \(optionalNumber!)")
 }
 
+//Optional Binding
+if let constantNumber = optionalNumber {
+    print("constant # = \(optionalNumber)")
+} else {
+    print ("optionalNumber is nil.")
+}
